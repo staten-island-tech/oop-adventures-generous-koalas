4e132c2f5976770954a4dcce79394dc957f8fe31
AnthonyC27 <anthonyc1018@nycstudents.net>
2023-11-29 13:58:46 -0500

    fsad

--- hero.py
+++ hero.py
@@ -2,12 +2,35 @@
 import json
 import os
 
+class Hero():        
+    def _init_(self, name, health, damage):
+        self.name = name
+        self.health = health
+        self,damage = damage
 
-class Dogs():
+
+class H (Hero):
     def create (self):
-     name= input("Enter name:")
-     damage= input("Enter damage:")
-     health= input("Enter health:")
-     cla ={"name":name, "size":damage,  "color":health}
-     return(cla)
-        
+        name = ("Cornelius")
+        health = ("200 hp")
+        damage = ("30 dmg")
+        return(name, health, damage)
+with open("data.json", "r") as f:
+    # Serialize the updated Python list to a JSON string
+    data = json.load(f)
+x = Hero()
+x1 = x.create()
+data.append(x1)
+#No code needed below this line
+# Creates a new JSON file with the updated data
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    # Serialize the updated Python list to a JSON string
+    json_string = json.dumps(data, indent=4)
+
+    # Write the JSON string to the new JSON file
+    f.write(json_string)
+
+# Overwrite the old JSON file with the new one
+os.remove("data.json")
+os.rename(new_file, "data.json")
