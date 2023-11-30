cb35bba5dbcf5b120f1bd4472075dea1da1d87b8
autumn curry <autumnc34@nycstudents.net>
2023-11-30 13:59:49 -0500

    storey like continued

---
+++ main.py
@@ -0,0 +1,4 @@
+from merchant import Merchant
+
+Audrey = Merchant("Audrey",["Sea-shell","green sweater", "Jarvis","Soul"])
+Audrey.sell("Soul") 
--- twst3.py
+++ twst3.py
@@ -10,5 +10,18 @@ def Woods_Start():
     if request in Path1: 
         print("As you continue walking down the road, you hear a weird sound. Is that...a snarl? A growl? Maybe it's a bear, or a raccoon but something hops out of the bushes. Something much worse than you expected. What do you do now??")
     if request in Path2: 
-        print("You found your way home. Game over…or is it? *ignore the 'None'")
+        print("You found your way home. Game over...or is it?")
 Woods_Start()
+
+
+
+Ans =  ["Piano", "a piano"]
+def Path1 (): 
+
+    print (" Once you look at their information, you take you and your weapon (sword) into battle. To win this battle you must answer the following question:")
+
+    riddle = input("I'm full of keys, but I cannot open a door, what am i? ")
+    if riddle in Ans:
+        print ("You are right! You won the battle against the baby zombie! Lets keep going!")
+    if riddle is not Ans: 
+        print ("Oh no! You died! D:")
