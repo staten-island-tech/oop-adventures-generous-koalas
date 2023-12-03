bbc7a573498995872d7569d92b8cf08d5b0b357b
Nat-bit <144480946+Nat-bit@users.noreply.github.com>
2023-12-03 16:17:54 -0500

    eruefufrydi

--- Zombies.py
+++ Zombies.py
@@ -6,22 +6,29 @@ class Zombie():
         name= input("Enter name: ")
         health= input("Enter health: ")
         damage= input("Enter damage: ")
-        poop={"name":name, "health":health, "damage":damage}
-        return(poop)
+        lo={"name":name, "health":health, "damage":damage}
+        return(lo)
 
 
 with open("data.json", "r") as f:
+    # Serialize the updated Python list to a JSON string
     data = json.load(f)
 
 a= Zombie()
 a1=a.create()
 data.append(a1)
 
+
+# Creates a new JSON file with the updated
+# data
 new_file = "updated.json"
 with open(new_file, "w") as f:
+    # Serialize the updated Python list to a JSON string
     json_string = json.dumps(data, indent=4)
 
+    # Write the JSON string to the new JSON file
     f.write(json_string)
 
+# Overwrite the old JSON file with the new one
 os.remove("data.json")
 os.rename(new_file, "data.json")
