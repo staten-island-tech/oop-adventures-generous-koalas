c61fda7b0f505e6b8445613538ad6cd08c692a48
Nat-bit <144480946+Nat-bit@users.noreply.github.com>
2023-12-07 14:00:51 -0500

    dfgsdfihsjilguopseruoph

--- fight.py
+++ fight.py
@@ -17,26 +17,23 @@ op5=("Fight","fight","f","F","FIGHT")
 
 
 def fight():
-    time.sleep(1.5)
     fstart
-    time.sleep(1.5)
     movedesc
-    time.sleep(1.5)
     zombie_health=200
     hero_health=200
     while hero_health>0:
         moveset=input ("-------\n1) Charged Attack, \n2) Normal Attack, \n3) Damage Potion,\n4)Healing Potion\n Enter the corresponding number:  ")
         if moveset== "1":
-            print(random.choice(op1))
-            if random.choice(op1) == ("Good job! The zombie's hp dropped by 30, that hit …"):
+            n = random.randint(1, 2)
+            if n == 1:
                 print ("Good job! The zombie's hp dropped by 30, that hit hurt...-15 hp")
                 hero_health = hero_health-15
                 zombie_health=zombie_health-30
                 print("Your health is..",hero_health)
                 time.sleep(1.5)
                 print("The zombie's health is",zombie_health)
-            if random.choice(op1) == ("You missed and the zombie attacked twice!!-20hp"):
-                print ("You missed and the zombie attacked twice!!-20hp")
+            elif n == 2:
+                print("You missed and the zombie attacked twice!!-20hp")
                 hero_health = hero_health-20
                 print("Your health is..",hero_health)
                 time.sleep(1.5)
@@ -46,27 +43,24 @@ def fight():
         if moveset=="2":
             print (random.choice(op2))
             if random.choice(op2) == ("Nice!The zombie's hp dropped by 15!"):
-                print("Nice!The zombie's hp dropped by 15!")
                 zombie_health=zombie_health-15
                 time.sleep(1.5)
                 print("The zombie's health is",zombie_health)
                 time.sleep(1.5)
-            if random.choice(op2) == ("You missed... the zombie bit you! -10hp"):
-                print("You missed... the zombie you!-10hp")
+            elif random.choice(op2) == ("You missed... the zombie bit you! -10hp"):
                 hero_health=hero_health-10
                 time.sleep(1.5)
                 print("Your health is....",hero_health)
                 time.sleep(1.5)   
 
         if moveset=="3":
-            print (random.choice(op3))
             if random.choice(op3) == ("You used your damage potion! The zombie's hp dropp…"):
                 print("Nice!The zombie's hp dropped by 30!")
                 zombie_health=zombie_health-30
                 time.sleep(1.5)
                 print("The zombie's health is",zombie_health)
                 time.sleep(1.5)
-            if random.choice(op3) == ("Your damage potion fell out of your hands atleast …"):
+            elif random.choice(op4) == ("Your potion fell out of your hands atleast you man…"):
                 print("Your damage potion fell out of your hands atleast you managed to dodge the zombie's attack....")
                 time.sleep(1.5)
                 print("Your health is....",hero_health)
@@ -75,16 +69,42 @@ def fight():
                 time.sleep(1.5)
 
         if moveset=="4":
-            if random.choice(op4) == ("You used your damage potion! The zombie's hp dropp…"):
-                print("Nice!The zombie's hp dropped by 15!")
-                zombie_health=zombie_health-30
+            if random.choice(op4) == ("You used your healing potion! +40 hp"):
+                print("You used your healing potion! +40p!")
+                hero_health=hero_health+40
                 time.sleep(1.5)
-                print("The zombie's health is",zombie_health)
+                print("Your health is..",hero_health)
                 time.sleep(1.5)
-            if random.choice(op4) == ("Your damage potion fell out of your hands atleast …"):
-                print("Your damage potion fell out of your hands atleast you managed to dodge the zombie's attack....")
+            elif random.choice(op4) == ("Your potion fell out of your hands atleast you man…"):
+                print("Your potion fell out of your hands atleast you managed to dodge the zombie's attack....")
                 time.sleep(1.5)
                 print("Your health is....",hero_health)
                 time.sleep(1.5)   
                 print("The zombie's health is",zombie_health)
-                time.sleep(1.5)
+
+        if zombie_health==0:
+            print("The Zombie died...")
+            break
+
+        if zombie_health<0:
+            print("HERO WINS.")
+            time.sleep(1.5)
+        
+        if hero_health==0:
+            print("You've died...")
+            break
+            
+        if hero_health<0:
+            print("ZOMBIE WINS.")
+            time.sleep(1.5)
+            fight_again= input("-------\nWould you like to battle again or go to the shop?\n'Fight' to try again.)")
+            if fight_again in op5:
+                return fight
+           
+    else:
+        print ("You didn't choose an option... Enter again: ")
+        still_continue =input("What attack do you want to use?")
+        moveset=still_continue
+print (fight())
+
+    
