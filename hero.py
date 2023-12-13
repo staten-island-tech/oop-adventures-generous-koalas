9375cf653557861041db5ee76ecd6a02bc656799
AnthonyC27 <anthonyc1018@nycstudents.net>
2023-12-13 13:55:02 -0500

    merchant

--- hero.py
+++ hero.py
@@ -1,17 +1,29 @@
 import random
 ## Create  Class for creating new dictionaries here
 class Hero():
-    def _init_(self,x,y,coins,weapon)
-        self.x=x
-        self.y=y
+    def _init_(self,health, name, coins, damage):
+        self.health=health
+        self.damage=damage
         self.coins=coins
-        self.weapon=weapon
-    def attack(self)
-        if self.weapon
-            return random.randint(1, self.weapon.damage)
+        self.name=name
+        self.inventory = {
+            "health_potions": [],
+            "damage_potions": [],
+        }
+    def use_health_potion(self):
+        if self.inventory['health_potions']:
+            potion = self.inventory['health_potions'].pop()
+            self.health += potion.healing
+            print(f"{self.name} used {potion.name}. Health restored by {potion.healing}.")
         else:
+            print("No health potions in inventory.")
 
-
-        
+    def use_damage_potion(self):
+        if self.inventory['damage_potions']:
+            potion = self.inventory['damage_potions'].pop()
+            self.damage += potion.damage_boost
+            print(f"{self.name} used {potion.name}. Damage boosted by {potion.damage_boost}.")
+        else:
+            print("No damage potions in inventory.")
 
         
