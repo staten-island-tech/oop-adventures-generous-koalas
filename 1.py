6de13bdae9062e2692c136dd6ee428dff1a8e7d0
AnthonyC27 <anthonyc1018@nycstudents.net>
2023-12-15 13:57:39 -0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,98 +1,109 @@
+
 import random
 
-class Potion:
-    def __init__(self, name, cost):
-        self.name = name
-        self.cost = cost
-
-class HealthPotion(Potion):
-    def __init__(self, name, healing, cost):
-        super().__init__(name, cost)
-        self.healing = healing
-
-class DamagePotion(Potion):
-    def __init__(self, name, damagee, cost):
-        super().__init__(name, cost)
-        self.damage = damagee
-
 class Hero:
-    def __init__(self, name, health, coins, damage):
+    def __init__(self, name, health, damage, coins):
         self.name = name
         self.health = health
-        self.coins = coins
         self.damage = damage
-        self.inventory = {
-            "health_potions": [],
-            "damage_potions": []
-        }
-    def use_health_potion(self):
-        if self.inventory['health_potions']:
-            potion = self.inventory['health_potions'].pop()
-            self.health += potion.healing
-            print(f"{self.name} used {potion.name}. Health restored by {potion.healing}.")
-        else:
-            print("No health potions in inventory.")
+        self.coins = coins
 
-    def use_damage_potion(self):
-        if self.inventory['damage_potions']:
-            potion = self.inventory['damage_potions'].pop()
-            self.damagee += potion.damagee
-            print(f"{self.name} used {potion.name}. Damage boosted by {potion.damagee}.")
-        else:
-            print("No damage potions in inventory.")
+    def attack(self):
+        return self.damage
 
+    def take_damage(self, damage):
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
 
-
-def earn_currency(self, amount):
-        self.currency += amount
+    def earn_coins(self, amount):
+        self.coins += amount
         print(f"{self.name} earned {amount} coins!")
 
-
-
-
-
+class Enemy:
+    def __init__(self, name, health, damage, reward):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.reward = reward
 
 class Merchant:
-    def __init__(self, available_items):
-        self.available_items = available_items
+    def __init__(self, items):
+        self.items = items
 
-    def offer_item(self):
+    def display_items(self):
         print("Merchant: Welcome, adventurer! Take a look at my wares:")
-        for idx, item in enumerate(self.available_items, start=1):
-            if isinstance(item, HealthPotion):
-                print(f"{idx}. {item.name} - Healing: {item.healing} - Cost: {item.cost} coins")
-            elif isinstance(item, DamagePotion):
-                print(f"{idx}. {item.name} - Damage Boost: {item.damagee} - Cost: {item.cost} coins")
-            else:
-                print(f"{idx}. {item}")
+        for idx, item in enumerate(self.items, start=1):
+            print(f"{idx}. {item['name']} - Cost: {item['cost']} coins")
 
+    def sell_item(self, hero):
+        self.display_items()
         choice = input("Enter the number of the item you want to purchase (or '0' to exit): ")
+
         try:
             choice = int(choice)
-            if 0 < choice <= len(self.available_items):
-                selected_item = self.available_items[choice - 1]
+            if 0 < choice <= len(self.items):
+                selected_item = self.items[choice - 1]
 
-                if isinstance(selected_item, Potion) and hero.coins >= selected_item.cost:
-                    hero.inventory[selected_item.name.lower() + "s"].append(selected_item)
-                    print(f"{hero.name} purchased {selected_item.name} for {selected_item.cost} coins. Added to inventory!")
-                    hero.currency -= selected_item.cost
-                elif isinstance(selected_item, Potion):
-                    print(f"{hero.name} doesn't have enough coins to buy {selected_item.name}.")
+                if selected_item['name'] == "Health Potion" or "Damage Potion" and hero.coins >= selected_item['cost']:
+                    hero.coins -= selected_item['cost']
+                    print(f"{hero.name} purchased {selected_item['name']} for {selected_item['cost']} coins.")
+                    return selected_item
                 else:
-                    print("Merchant: Sorry, I don't sell that item.")
+                    print(f"{hero.name} doesn't have enough coins to buy {selected_item['name']}.")
             elif choice != 0:
                 print("Invalid choice. Please enter a valid number.")
         except ValueError:
             print("Invalid input. Please enter a valid number.")
-        return None  
-    print("Inventory:")
-    print(f"Health Potions: {len(Hero.inventory['health_potions'])}")
-    print(f"Damage Potions: {len(Hero.inventory['damage_potions'])}")
 
+        return None
 
+def print_status(hero, zombies):
+    print(f"{hero.name}'s Health: {hero.health} | Coins: {hero.coins}")
+    print("Zombies:")
+    for zombie in zombies:
+        print(f"{zombie.name} - Health: {zombie.health} | Damage: {zombie.damage}")
 
-hero = Hero(name="John", health=200, coins=50, damage=15)
-merchant = Merchant(available_items=[
-    HealthPotion(name="Health Potion", healing=5, cost=25),
-    DamagePotion(name="Damage Potion", damagee=30, cost=25),
-])
+def hero_turn(hero, zombies, merchant):
+    print("\nHero's Turn:")
+    print_status(hero, zombies)
+
+    print("1. Continue")
+    print("2. Visit Merchant")
+    choice = input("Enter your choice (1 or 2): ")
+
+    if choice == '1':
+        print("You move on")
+
+    elif choice == '2':
+        item = merchant.sell_item(hero)
+        if item:
+            print(f"{hero.name} received a {item['name']}!")
+
+def zombies_turn(hero, zombies):
+    print("\nZombies' Turn:")
+    for zombie in zombies:
+        damage_taken = zombie.damage
+        hero.take_damage(damage_taken)
+        print(f"{zombie.name} attacks {hero.name} and deals {damage_taken} damage!")
+
+def main():
+    hero = Hero(name="Cornelius", health=200, damage=15, coins=50)
+    zombies = [Enemy(name="Zombie1", health=100, damage=10, reward=20)]
+    merchant_items = [
+        {"name": "Health Potion", "cost": 25},
+        {"name": "Damage Potion", "cost": 25},
+    ]
+    merchant = Merchant(items=merchant_items)
+
+    while hero.health > 0 and zombies:
+        hero_turn(hero, zombies, merchant)
+        zombies_turn(hero, zombies)
+
+    if hero.health <= 0:
+        print(f"{hero.name} has been defeated. Game over!")
+    else:
+        print(f"{hero.name} defeated all the zombies! You win!")
+
+if __name__ == "__main__":
+    main()
