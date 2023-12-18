593aa9bac468bc6a110cbfcc5cb674c5baba64e4
autumn curry <autumnc34@nycstudents.net>
2023-12-18 13:59:18 -0500

    wsrk m 8hgnbfvc gdtyf hutjk .,

--- test.py
+++ test.py
@@ -3,21 +3,12 @@ def start():
     b = input("Hello! You are a student in Staten Island Technical High School and you are attempting to survive the zombie apocalypse.")
     c = input ("The goal is to get to Staten Island Techincal high School's roof, where the rescue point is. You will start in Level 1, in the basement. Be careful!") 
     start = input("Your chemistry teacher's alarm rings, indicating that tutoring sess is over! You begin to get up. Do you want to go to the Comp Sci room or SO Store")
-    BeginningA = ["So store", "school store", "School Store", "Snack Store"]
+    BeginningA = ["SO store", "school store", "School Store", "Snack Store", "So Store"]
     BeginningB = ["Computer Sci Room", "Comp Sci room", "Comp Sci", "comp sci", "Mr whalen's room"]
     if start in BeginningA:
-         print ("What do you do in the SO Store?")
+         print ("You chose the School Store!")
+         import compsci
     elif start in BeginningB:
-         print("You walk in and realize that no one is in the room")
+         print("You chose the CompSci Room!")
+         import schoolstore
 start()
-def CompSci():
-     d = input("You can either look around or finish your code from earlier. Which one would you like to do?") 
-     if d in Nonfight1:
-          print("As you nearly finish your code, you hear the door creak and a small growl. What is that?")
-     if d in Nonfight2: 
-          print ("As you look near the back of the room, you hear the door creak.")
-
-Nonfight1 = [" Finish your code from earlier"]
-Nonfight2 = ["Look around"]
-
-CompSci()
