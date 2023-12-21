705d6ab2906c1143e2d0cc25805bfd005180529f
autumn curry <autumnc34@nycstudents.net>
2023-12-21 12:52:17 -0500

    only bc im switchiung branches

--- schoolstore.py
+++ schoolstore.py
@@ -3,4 +3,12 @@ def SchoolStore():
     y = print("You walk into the pantry where they store all the cookies, gummies, sour patch kids, and skittles.")
     z = print("You start rummaging through the pantry, looking for your favorite candy")
     zombie1 = print("After you found your snack, and go back to the main room, you realize the door is slightly opened...uh oh") 
+
+
+
+
+
+
+
+
 SchoolStore() 
