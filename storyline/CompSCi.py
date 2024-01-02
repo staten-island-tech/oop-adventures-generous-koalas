0676c8f31d15fd6ffb61f3c9068b71d402454bde
autumn curry <144480668+autumnc14@users.noreply.github.com>
2024-01-01 22:10:10 -0500

    Update CompSCi.py

---
+++ storyline/CompSCi.py
@@ -0,0 +1,7 @@
+def CompSciStory(): 
+a = "As you walk in , you realize nobody is in the room currently." 
+b = input("You can either open your computer and finish coding or you can go towards the back, if you'd like. Input Code for the first choice. Input Back for the second. Keep C and B uppercase.")
+if b == "Code": 
+  print("you walk..") 
+if b == "Back": 
+  print("you now have..") 
