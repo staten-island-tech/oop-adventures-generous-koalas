6cc472dc78ef19e4a8848970454c3c523a358c78
autumn curry <144480668+autumnc14@users.noreply.github.com>
2024-01-01 22:04:12 -0500

    Update storylinetested.py

--- storyline/storylinetested.py
+++ storyline/storylinetested.py
@@ -5,10 +5,13 @@ def start():
     start = input("Your chemistry teacher's alarm rings, indicating that tutoring sess is over! You begin to get up. Do you want to go to the Comp Sci room or SO Store")
     BeginningA = ["So store", "school store", "School Store", "Snack Store"]
     BeginningB = ["Computer Sci Room", "Comp Sci room", "Comp Sci", "comp sci", "Mr whalen's room"]
-    if start in BeginningA:
-         print ("What do you do in the SO Store?")
-    elif start in BeginningB:
-         print("You walk in and realize that no one is in the room")
-         class CompSCi(): 
-              
+    if start in BeginningB:
+        print ("you are now in the computer science room, what would you like to do?")
+        import CompSCi.py
+    if start in BeginningC:
+        print ("you are now in the band room") 
+        import Bandroom.py
+    elif:
+        print ("you are going to the so store")
+        import merchant.py
 # make a class that represents the so store, and the choices they can make. 
