5f231bdcb7deb50c0d850478f06db6dfd8027054
Nat-bit <144480946+Nat-bit@users.noreply.github.com>
2024-01-12 13:42:05 -0500

    12369

--- app.py
+++
@@ -1,7 +0,0 @@
-
-## IN THIS FILE YOU WILL ONLY READ FROM THE JSON DATA USING CLASSES
-import json
-# Open the JSON file of pokemon data
-test = open("data.json", encoding="utf8")
-# create variable "data" that represents the enitre pokedex list
-data = json.load(test)
--- frijoles.py
+++
@@ -1,3 +0,0 @@
-from hero import Hero
-from Zombies import Enemy
-from beans import *
