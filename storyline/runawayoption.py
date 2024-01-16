d3ec43dac3586cf691167298f44533603cea0962
autumn curry <144480668+autumnc14@users.noreply.github.com>
2024-01-15 22:33:04 -0500

    Update runawayoption.py

--- storyline/runawayoption.py
+++ storyline/runawayoption.py
@@ -4,7 +4,7 @@ class runaway:
   print(" You found out two hiding spots! The flute cabinet, which is completely empty right now or the back of the room, behind the bass drum!") 
   start1 = input(" (”Now you must go to the school store to get weapons, potions, etc \n You have a 30$ worth weapon! Hopefully it will make some damages! Press Enter to continue") 
   hallway = input(”you open the further down band room door, trying not to make any sound. There’s no sign of a zombie in the hallway. \n Once you silently slip out, you begin fastly tiptoeing to the school store. \n Once you are in the room, you lock the door behind you. Luckily, there is an owner that’s alive but fearful. \n He’s immortal so he cannot die no matter what.”)
-  schoolstore() / inventory () wtv they come up with 
+  ## import schoolstore() / inventory () wtv they come up with 
 
  ##this is after their purchase!
 schoolstoreleave = print (”great! Now you have all the weapons and potions you need! Let’s go back to the band room!!”) 
