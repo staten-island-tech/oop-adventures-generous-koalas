683840316277dba00a95fd0f7869a5c5b18c645b
autumn curry <144480668+autumnc14@users.noreply.github.com>
2024-01-15 22:21:55 -0500

    Create runawayoption.py

---
+++ storyline/runawayoption.py
@@ -0,0 +1,7 @@
+class runaway: 
+  def RunOption(): 
+  print("You returned back to the band room, hiding in Mr. Rams’ room, pondering on what to do next. \n  Well first you’d obviously would need a good hiding place and a weapon. \n luckily for you, the school store is right outside! \n To get more weapons you must go outside… \n Stay Safe! In the mean time, you are rewarded with a retired flute \n in danger, humans would do anything to save themselves. what would you do?”) 
+  print(" You found out two hiding spots! The flute cabinet, which is completely empty right now or the back of the room, behind the bass drum!") 
+  start1 = input(" (”Now you must go to the school store to get weapons, potions, etc \n You have a 30$ worth weapon! Hopefully it will make some damages! Press Enter to continue") 
+  hallway = input(”you open the further down band room door, trying not to make any sound. There’s no sign of a zombie in the hallway. \n Once you silently slip out, you begin fastly tiptoeing to the school store. \n Once you are in the room, you lock the door behind you. Luckily, there is an owner that’s alive but fearful. \n He’s immortal so he cannot die no matter what.”)
+  
