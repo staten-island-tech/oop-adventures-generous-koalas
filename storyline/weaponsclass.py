b1a539b8022132f263170e64a4432a5dd2ad67b5
autumn curry <144480668+autumnc14@users.noreply.github.com>
2024-01-15 19:48:55 -0500

    Update weaponsclass.py

--- storyline/weaponsclass.py
+++ storyline/weaponsclass.py
@@ -3,38 +3,15 @@ class Weapons:
         self.name = name 
         self.damage = damage
         self.coins = coins
-Troika = Weapons("Troika", -1, 0 )
-
-Dull_Pencil = Weapons("Dull Pencil", 16, 5)
 
 Sharpened_Pencil = Weapons("Sharpened Pencil", 25, 15)
-
 Safety_Scissors = Weapons("Safety Scissors", 30, 20)
-
 Opened_Paperclip = Weapons ("Opened Paperclip", 45, 30)
-
 Heavy_Backpack = Weapons("Heavy Backpack", 40, 40 )
-
-Shoes = Weapons("A kids lost and found shoe", 2, 10)
-
 Metal_Waterbottle = Weapons ("Metal Waterbottle", 60, 45)
-
 Damaged_Potion = Weapons("Damage Potion", 100 , 125)
-
 Broken_Computer = Weapons ("Broken Computer", 5, 20) 
-
 Computer_Wires = Weapons ("Electric Computer wires", 30, 70)
-
 Broken_Glasses = Weapons ("Broken Glass Lens", 55, 70) 
-
 LandYard = Weapons("Land Yard", 12, 30) 
-
-Masks = Weapons ("Masks", 15, 40) 
-
 HairClip = Weapons("Hair Clip", 20, 50) 
-
-RetiredFlute = Weapons ("Retired Flute", 25, 55)
-
-RetiredTrombone = Weapons ("Retired Trombone", 30, 60) 
-
-RetiredViolinBow = Weapons ("Retired Violin Bow", 45, 70)
