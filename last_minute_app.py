d469fdeaf9fb58cb9b2faf8c94bed50a790e5303
autumn curry <144480668+autumnc14@users.noreply.github.com>
2024-01-16 01:53:44 -0500

    Update last_minute_app.py

--- last_minute_app.py
+++ last_minute_app.py
@@ -1,25 +1,25 @@
 
-from normalzombie import p1
-from babyzombie import p2
-from mamazombie import p3
-from babyzombietroika import p5
-from troikaweapon import p4
-from mamazombietroika import p6
-from babydullpencil import p8
-from dullpencil import p7
-from dullpencilmamazomb import p9
-from sharpened import p10
-from babyzombiesharpenedpencil import p11
-from mamazombiesharmpenedpencil import p12
-from safetyscissors import p13
-from safteyscissors2 import p14
-from safteyscissors3 import p15
-from openedpaperclip import p16
-from openedpaperclip2 import p17
-from openedpaperclip3 import p18
-from heavypackpack import p19
-from babyzombieheavybackpack2 import p20
-from backpack3 import p21
+from normalzombie import LEVEL2 import p1
+from babyzombie import storyline import bandstoryline import compscistoryline import backofcompsci import computercompsci import BANDrunawayoption import p2
+from mamazombie import LEVEL3 import p3
+from babyzombietroika import storyline import bandstoryline import compscistoryline import backofcompsci import computercompsci import BANDrunawayoption import p5
+from troikaweapon import LEVEL2 import p4
+from mamazombietroika import LEVEL3 import p6
+from babydullpencil import storyline import bandstoryline import compscistoryline import backofcompsci import computercompsci import BANDrunawayoption import p8
+from dullpencil import LEVEL2 import p7
+from dullpencilmamazomb import LEVEL3 import p9
+from sharpened import LEVEL2 import p10
+from babyzombiesharpenedpencil import storyline import bandstoryline import compscistoryline import backofcompsci import computercompsci import BANDrunawayoption import p11
+from mamazombiesharmpenedpencil import LEVEL3 import p12
+from safetyscissors import storyline import bandstoryline import compscistoryline import backofcompsci import computercompsci import BANDrunawayoption import p13
+from safteyscissors2 import LEVEL2 import p14
+from safteyscissors3 import LEVEL3 import p15
+from openedpaperclip import storyline import bandstoryline import compscistoryline import backofcompsci import computercompsci import BANDrunawayoption import p16
+from openedpaperclip2 import LEVEL2 import p17
+from openedpaperclip3 import LEVEL3 import p18
+from heavypackpack import LEVEL2 import p19
+from babyzombieheavybackpack2 import storyline import bandstoryline import compscistoryline import backofcompsci import computercompsci import BANDrunawayoption import p20
+from backpack3 import LEVEL3 import p21
 import random
 import time
 
