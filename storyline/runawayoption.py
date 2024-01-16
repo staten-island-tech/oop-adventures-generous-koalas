2093a8dc7771858eff92f671153b0d0ae965f650
autumn curry <144480668+autumnc14@users.noreply.github.com>
2024-01-15 22:32:27 -0500

    Update runawayoption.py

--- storyline/runawayoption.py
+++ storyline/runawayoption.py
@@ -4,4 +4,14 @@ class runaway:
   print(" You found out two hiding spots! The flute cabinet, which is completely empty right now or the back of the room, behind the bass drum!") 
   start1 = input(" (”Now you must go to the school store to get weapons, potions, etc \n You have a 30$ worth weapon! Hopefully it will make some damages! Press Enter to continue") 
   hallway = input(”you open the further down band room door, trying not to make any sound. There’s no sign of a zombie in the hallway. \n Once you silently slip out, you begin fastly tiptoeing to the school store. \n Once you are in the room, you lock the door behind you. Luckily, there is an owner that’s alive but fearful. \n He’s immortal so he cannot die no matter what.”)
-  
+  schoolstore() / inventory () wtv they come up with 
+
+ ##this is after their purchase!
+schoolstoreleave = print (”great! Now you have all the weapons and potions you need! Let’s go back to the band room!!”) 
+hallway2 = input(”As you start walking towards the band room’s door you realized that it is cracked open. press Enter to continue.”) 
+introtofight = print(”you crept in, and see a zombie, probably the same one from earlier, rummaging around the room, groaning. \n you have two hiding places. Where would you like to hide? Type “1” for the flute cabinet, Type “2” for the behind the bass drum”) 
+if introtofight == “1”: 
+print(”You quickly tip toe to the flute cabinet, crouch and bring your knees close to your chest \n The zombie begins sniffing around, noticing that a new scent (human) has entered the room. \n he then gets closer to the flute cabinet \n Holding your breath, you hope they he doesn’t know you are in here, and hoping he does not open the door.”) 
+a = input(”You heart is beginning to race and you are clutching on to your newly brought weapon/potion. He then looks at the cabinet…would you like to know what happens next? Press ENTER to continue.”) 
+b = input(”The zombie then groans, and starts walking out the room…He didn’t realize you were in there! Thank god you chose to hide in the flute cabinet!!”) 
+if introtofight == “2
