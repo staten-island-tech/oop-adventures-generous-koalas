e426cb19a361072eb20145868c2256fb1db56fee
autumn curry <144480668+autumnc14@users.noreply.github.com>
2024-01-16 00:36:08 -0500

    Update level2.py

--- storyline/level2.py
+++ storyline/level2.py
@@ -1,5 +1,10 @@
 class level2 
 def Secondfloor(): 
-  print("You are now on the second floor! Both the third and first floor are now opened to you!") 
-  
+  print("You are now on the second floor! Now the whole second and first floor are now opened to you!") 
+  start = input("As you walk down the hallway, swiftly and quietly, you noticed Ms. Ravitz' room was cracked open.\n You peaked in, noticing all the papers stashed on her desk. Press ENTER to continue")
+  begin = input("You sit down in her chair, begin searching the papers, trying to find a way to get out of this hellhole. \n Then you see a paper in cardstock saying 'ESCAPE = ROOF'. \n You need to go to the roof! Lucily, staircase I, takes you DIRECTLY to the roof!/n But...how to get there...with no harm? Press ENTER to continue")
+  print("as you walk out, you hear a LOUD growling sound. Uh oh! You begin running quietly across the halls, making your way to the staircase, hoping nothing or no ONE is behind you!") 
+  print("Once you reached halfway there, you see a figure. A large, stick-like figure. Oh no! \n The figure abrutly stops and looks over to you. And then starts running!") 
+  weapons = input("It;s charging at you! Which weapon would you like to choose?  
+
   
