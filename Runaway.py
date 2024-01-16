2334670dceec865cd890d4d66ec546f379c985a6
autumn curry <144480668+autumnc14@users.noreply.github.com>
2024-01-15 19:41:35 -0500

    Create Runaway.py

---
+++ Runaway.py
@@ -0,0 +1,17 @@
+class Runaway:
+  def RunOption(): 
+  print("you returned back to the band room, hiding in Mr. Rams' room, pondering on what to do next. \n Well first you'd obviously would need a good hiding place and a weapon. \n Luckily for you, the school store is right outside! \n To get more weapons you must go outside... \n Stay Safe In the mean time, you are rerwarded with a retired flute \n In danger, humans would do anything to save themselves. What would you do?") 
+  print("You found out two great hiding spots! The flute cabinet which is completely empty right now, or the back of the room behind the bass drum!") 
+
+start1 = input("Now you must go to the school store to get weapons, potions, etc \n You have a 30$ worth weapon! Hopefully it will make some damages! Press ENTER to continue.")
+
+hallway = input(" you open the further down band room door, trying not to make any sound. There’s no sign of a zombie in the hallway. \n Once you silently slip out, you begin fastly tiptoeing to the school store. \n Once you are in the room, you lock the door behind you. Luckily, there is an owner that’s alive but fearful. \n He’s immortal so he cannot die no matter what.")
+import schoolstore()
+
+schoolstoreleave = print (" great ! now you have all the weapons/potions you need! Let's go back to the bandroom!") 
+hallway2 = input(”As you start walking towards the band room’s door you realized that it is cracked open. press Enter to continue.”) 
+introtofight = print(”you crept in, and see a zombie, probably the same one from earlier, rummaging around the room, groaning. \n you have two hiding places. Where would you like to hide? Type “1” for the flute cabinet, Type “2” for the behind the bass drum.")
+if introtofight == "1":
+  print("you crept in, and see zombie, probably the same one from earlier, rummaging around the room, groaning. \n You have two hiding places. Where would you like to hide? Type 
+"1" for the flute cabinet, Type "2" for the behind the bass drum") 
+  
