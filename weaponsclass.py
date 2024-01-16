b50cccb4064c6c951113fc673af0b6d2461c5c5e
autumn curry <144480668+autumnc14@users.noreply.github.com>
2024-01-15 19:53:25 -0500

    Update weaponsclass.py

--- weaponsclass.py
+++ weaponsclass.py
@@ -3,8 +3,6 @@ class Weapons:
         self.name = name 
         self.damage = damage
         self.coins = coins
-Troika = Weapons("Troika", -1, 0 )
-Dull_Pencil = Weapons("Dull Pencil", 16, 5)
 Sharpened_Pencil = Weapons("Sharpened Pencil", 25, 15)
 Safety_Scissors = Weapons("Safety Scissors", 30, 20)
 Opened_Paperclip = Weapons ("Opened Paperclip", 45, 30)
